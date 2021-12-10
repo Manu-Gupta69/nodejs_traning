bd7b93f18c0c829a7cc127b82082a111b1c1b4b8
coder biceps <manugupta619@gmail.com>
2021-12-10 12:25:09 +0530

    ..

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const inventoryRoutes = require('./routes/inventory');
 app.use(express.json());
 app.use(inventoryRoutes);
 
-app.listen(3000 , ()=>{
-    console.log('listening on port 3000')
-})
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
