b33cd40b5e1c202eb70331fac5efbd57c8726e5a
coder biceps <manugupta619@gmail.com>
2021-12-10 12:25:51 +0530

    ..

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -9,20 +9,22 @@ exports.getInventorydata = (req, res, next) => {
     } else {
       data = JSON.parse(filecontent);
     }
-    res.json({"data" : data});
+    res.json({ data: data });
   });
 };
 exports.postInventorydata = (req, res, next) => {
   const reqdata = req.body;
-  fs.readFile(datapath , (err ,filecontent)=>{
-      let inventory = [];
-      if(err){
-          inventory.push(reqdata);
-      }else{
-          inventory = JSON.parse(filecontent);
-          inventory.push(reqdata);
-      }
-      fs.writeFile(datapath , JSON.stringify(inventory) , (err , content)=> console.log(err));
-      res.json({"data" : inventory , "status" : "successfully added"});
-  })
+  fs.readFile(datapath, (err, filecontent) => {
+    let inventory = [];
+    if (err) {
+      inventory.push(reqdata);
+    } else {
+      inventory = JSON.parse(filecontent);
+      inventory.push(reqdata);
+    }
+    fs.writeFile(datapath, JSON.stringify(inventory), (err, content) =>
+      console.log(err)
+    );
+    res.json({ data: inventory, status: 'successfully added' });
+  });
 };
