03896b0bbec15cf72c3821740d9339b8b17965a2
coder biceps <manugupta619@gmail.com>
2021-12-11 17:33:31 +0530

    convert normal password to hashed password to store in database

--- controller/auth.js
+++ controller/auth.js
@@ -1,13 +1,15 @@
 const authSchema = require("../util/authSchema");
 const User = require("../model/user");
+const bcrypt = require("bcryptjs");
 
 exports.postSignup = async (req, res, next) => {
   try {
     const data = req.body;
     await authSchema.signupSchema.validateAsync(data);
-    const user = new User(data.username, data.name, data.password, data.email);
+    const hashedpassword = await bcrypt.hash(data.password, 12);
+    const user = new User(data.username, data.name, hashedpassword, data.email);
     await user.save();
-    res.status(200).json({ data: "usersuccessfully created" });
+    res.status(200).json({ data: "user created successfully" });
   } catch (err) {
     console.log(err);
     res.status(400).send(err);
