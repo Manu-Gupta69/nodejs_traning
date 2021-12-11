b629111eb9f122e3ebadbb4f19d65b50f76fd92e
coder biceps <manugupta619@gmail.com>
2021-12-11 19:15:10 +0530

    add findone class function and refactor signup controller accordingly

--- controller/auth.js
+++ controller/auth.js
@@ -6,14 +6,20 @@ exports.postSignup = async (req, res, next) => {
   try {
     const data = req.body;
     await authSchema.signupSchema.validateAsync(data);
-    const hashedpassword = await bcrypt.hash(data.password, 12);
-    const user = new User(data.username, data.name, hashedpassword, data.email);
+    const hashedPassword = await bcrypt.hash(data.password, 12);
+    const user = new User(data.username, data.name, hashedPassword, data.email);
     await user.save();
-    res.status(200).json({ data: "user created successfully" });
+    res.status(200).json({ err: null, data: "user created successfully" });
   } catch (err) {
     console.log(err);
-    res.status(400).send(err);
+    res.status(400).json({ err, data: null });
   }
 };
 
-exports.postLogin = (req, res, next) => {};
+exports.postLogin = async (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  const result = await User.findOne("email", data.email);
+
+  res.send(true);
+};
--- model/user.js
+++ model/user.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const crypto = require("crypto");
-
+const asyncfs = require("fs/promises");
 const databasePath = path.join(__dirname, "..", "database", "data.json");
 
 class User {
@@ -10,6 +9,7 @@ class User {
     this.name = name;
     this.password = password;
     this.email = email;
+    this.createdAt = new Date().toISOString();
   }
 
   writeFile(path, contentToSave) {
@@ -23,11 +23,29 @@ class User {
     });
   }
 
+  static findOne(criteria, toBeFound) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const dataContent = await asyncfs.readFile(databasePath);
+        const result = JSON.parse(dataContent).findIndex((user) => {
+          return user[criteria] === toBeFound;
+        });
+        if (result >= 0) {
+          resolve(JSON.parse(dataContent)[result]);
+        } else {
+          resolve(null);
+        }
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
   save() {
     return new Promise((resolve, reject) => {
       let content = [];
       this.id = content.length;
-      fs.readFile(databasePath, (err, filecontent) => {
+      fs.readFile(databasePath, async (err, filecontent) => {
         if (err) {
           content.push(this);
           this.writeFile(databasePath, content)
@@ -35,11 +53,10 @@ class User {
             .catch((err) => reject(err));
         } else {
           content = JSON.parse(filecontent);
-          const result = content.findIndex((user) => {
-            return user.email === this.email;
-          });
-          if (result >= 0) {
-            reject("user already exists");
+          const result = await User.findOne("email", this.email);
+          console.log(result);
+          if (result !== null) {
+            reject("User Already Exits");
             return;
           }
           this.id = content.length;
